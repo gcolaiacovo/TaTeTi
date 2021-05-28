8b1f7e2535e2e2d3595d703341f0e7d1f58742e9
Gino Colaiacovo <gino.colaiacovo@gmail.com>
2021-05-27 23:53:55 -0300

    first commit

---
+++ TaTeTi/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace TaTeTi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.lblMovimientos = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTurno.Location = new System.Drawing.Point(631, 36);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(0, 18);
+            this.lblTurno.TabIndex = 0;
+            // 
+            // lblMovimientos
+            // 
+            this.lblMovimientos.AutoSize = true;
+            this.lblMovimientos.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMovimientos.Location = new System.Drawing.Point(605, 129);
+            this.lblMovimientos.Name = "lblMovimientos";
+            this.lblMovimientos.Size = new System.Drawing.Size(0, 18);
+            this.lblMovimientos.TabIndex = 1;
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(614, 357);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(112, 41);
+            this.btnReiniciar.TabIndex = 2;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(614, 425);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(112, 41);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 512);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.lblMovimientos);
+            this.Controls.Add(this.lblTurno);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ta-Te-Ti by GFCSoft";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.Label lblMovimientos;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ TaTeTi/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaTeTi
+{
+    public partial class Form1 : Form
+    {
+        List<Button> listaBotones = new List<Button>();
+        int Turno = 0;
+        int Movimientos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CrearBotones();
+            ActualizarLabelTurno();
+        }
+
+        void CrearBotones()
+        {
+            int left = 50;
+            int top = 50;
+
+            for (int index = 0; index < 9; index ++)
+            {
+                var boton = new Button();
+                boton.Width = 80;
+                boton.Height = 80;
+                boton.Font = new Font(new FontFamily("Verdana"), 18);
+                boton.Visible = true;
+                boton.Left = left;
+                boton.Top = top;
+                boton.Click += OnClickBoton;
+                left += 150;
+
+                if (index == 2 || index == 5)
+                {
+                    top += 150;
+                    left = 50;
+                }
+
+                listaBotones.Add(boton);
+                this.Controls.Add(boton);
+            }
+        }
+
+        void OnClickBoton(object sender, EventArgs eventArgs)
+        {
+            Button boton = (Button)sender;
+
+            if (string.IsNullOrEmpty(boton.Text) && !ChequeaGanador())
+            {
+                boton.Text = Turno == 0 ? "X" : "O";
+                if (ChequeaGanador())
+                {
+                    if (MessageBox.Show($"Ganó el jugador {Turno + 1}. Desea jugar de nuevo?", "Ganador", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        ReiniciarJuego();
+                    }
+                }
+                else
+                {
+                    if (Movimientos < 8)
+                    {
+                        Turno = Turno == 0 ? 1 : 0;
+                        ActualizarLabelTurno();
+                        Movimientos++;
+                        lblMovimientos.Text = "Movimientos: " + Movimientos;
+                    }
+                    else
+                    {
+                        if (MessageBox.Show($"Hubo un empate. Desea jugar de nuevo?", "Empate", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            ReiniciarJuego();
+                        }
+                    }
+                }
+            }
+        }
+
+        void ActualizarLabelTurno()
+        {
+            lblTurno.Text = "Turno: Jugador " + (Turno + 1);
+        }
+
+        bool ChequeaGanador()
+        {
+            if ((!string.IsNullOrEmpty(listaBotones[0].Text) && listaBotones[0].Text == listaBotones[1].Text && listaBotones[1].Text == listaBotones[2].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[3].Text) && listaBotones[3].Text == listaBotones[4].Text && listaBotones[4].Text == listaBotones[5].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[6].Text) && listaBotones[6].Text == listaBotones[7].Text && listaBotones[7].Text == listaBotones[8].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[0].Text) && listaBotones[0].Text == listaBotones[3].Text && listaBotones[3].Text == listaBotones[6].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[1].Text) && listaBotones[1].Text == listaBotones[4].Text && listaBotones[4].Text == listaBotones[7].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[2].Text) && listaBotones[2].Text == listaBotones[5].Text && listaBotones[5].Text == listaBotones[8].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[0].Text) && listaBotones[0].Text == listaBotones[4].Text && listaBotones[4].Text == listaBotones[8].Text) ||
+                (!string.IsNullOrEmpty(listaBotones[2].Text) && listaBotones[2].Text == listaBotones[4].Text && listaBotones[4].Text == listaBotones[6].Text))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ReiniciarJuego()
+        {
+            Turno = 0;
+            Movimientos = 0;
+            lblMovimientos.Text = "Movimientos: " + Movimientos;
+
+            for (int index = 0; index < 9; index ++)
+            {
+                listaBotones[index].Text = "";
+            }
+
+            ActualizarLabelTurno();
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            ReiniciarJuego();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
